ff849d7b5d3b0642e3cd46748f164fb40ba2f004
wbglaeser <ben.glaeser@tuta.io>
2019-08-08 18:04:53 +0200

    start building input actor

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ use actix::{Actor, Addr, System, Message, Arbiter};
 use futures::{future, Future};
 use failure::Fail;
 use std::{thread, time};
+use std::io::{stdin, Stdin};
+
 
 mod actors;
 
 use actors::renderer::{Rendact, Commands, Command, Coordinate, GameBoard, Id};
+use actors::inputer::{Inputact, ICMD, CMDEN};
 
 fn main() {
        
@@ -15,19 +18,21 @@ fn main() {
 
     // Set up Render Actor
     let mut coos = SyncArbiter::start(2, || Rendact::new());
-            
-
     let fut = coos.send(Command(Commands::Welcome))
         .map_err(|err| ())
         .and_then(|x| { future::ok(()) });
     
-    Arbiter::spawn(fut);
-    
-    let fut2 = coos.send(Command(Commands::Goodbye))
+    Arbiter::spawn(fut);    
+
+    // Set up Input Actor
+    let mut inps = SyncArbiter::start(2, || Inputact::new());
+    let stdin = stdin();
+    let fut2 = inps.send(ICMD(CMDEN::SETUP))
         .map_err(|err| ())
         .and_then(|x| { future::ok(()) });
-    
+
     Arbiter::spawn(fut2);
 
+    // Run this stuff
     sys.run();
 }
