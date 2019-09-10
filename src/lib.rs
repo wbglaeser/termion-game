475a4e556e11210eedbbc2cba2d992f19f844583
wbglaeser <ben.glaeser@tuta.io>
2019-09-10 16:43:42 +0200

    refactor input model to read asyncly

--- src/lib.rs
+++ src/lib.rs
@@ -1,358 +1,64 @@
-use std::io::{Error, ErrorKind};
-use failure::Fail;
-use std::fmt;
-use actix::{Actor,Addr, Handler, Message, SyncContext, System, Context};
-use std::collections::HashMap;
-use std::io::{Write, stdin, stdout};
+use std::io;
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
+use std::{thread, time};
 
+use termion;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use termion::event::Key;
-
-pub type Id = i16;
-pub type EntityIndex = i16;
-
-//
-// Actors
-//
-
-// Render Engine
-pub struct Rendact {}
-
-// Design style elements
-const GAME_WELCOME: &'static str = "╔══════════════════════════════════════╗\n\r\
-                                    ║───┬Welcome to this beautiful Game────║\n\r\
-                                    ║ p ┆ play                             ║\n\r\
-                                    ║ q ┆ quit                             ║\n\r\
-                                    ╚═══╧══════════════════════════════════╝";
-
-const GAME_FIELD: &'static str = "╔══════════════════════════════════════╗\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ║                                      ║\n\r\
-                                  ╚══════════════════════════════════════╝";
 
 
-impl Actor for Rendact {
-    type Context = SyncContext<Self>;
-}
+pub fn spawn_stdin_channel() -> Receiver<termion::event::Key> {
 
-impl Rendact {
-    pub fn new() -> Self {
-        Self {
-        }
-    }
+    let mut stdin = termion::async_stdin().keys();
+    let (tx, rx) = mpsc::channel::<termion::event::Key>();
 
-    pub fn welcome_message(&mut self) {
-                
-        // Welcome message
-        println!("{}{}{}{}",
-            termion::clear::All,
-            termion::cursor::Goto(1,1),
-            GAME_WELCOME,
-            termion::cursor::Hide);
-    }
+    thread::spawn(move || {
+        loop {
+            let tx = tx.clone();
+            let input = stdin.next();
 
-    pub fn goodbye_message(&mut self) {
-        
-        // Welcome message
-        println!("{}Well that was a lot of fun. See you next time.{}",
-            termion::cursor::Goto(1,4),
-            termion::cursor::Hide);
-
-    }
-
-    pub fn place_player(&mut self, pos: &Position) {
-
-        println!("{}O",
-            termion::cursor::Goto(pos.x as u16, pos.y as u16));
-
-    }
-}
-
-// Message Sectioni
-pub enum Commands {
-    Welcome,
-    Goodbye,
-}
-
-pub struct Command (pub Commands);
-
-impl Message for Command {
-    type Result = Value;
-}
-
-impl Handler<Command> for Rendact {
-    type Result = Value;
-
-    fn handle(&mut self, Command(msg): Command, _: &mut  SyncContext<Self>) -> Self::Result {
-        let reaction = match msg {
-            Commands::Welcome => {
-                self.welcome_message(); 
-            },
-            Commands::Goodbye => {
-                self.goodbye_message();
-                System::current().stop();
-            }
-        };
-        0    
-    }
-}
-
-// Player
-pub struct Player {
-    pub id: Id,
-    pub physics: Physics,
-    pub health: i16,
-    pub entity_type: EntityType,
-}
-
-type Value = u64;
-
-impl Actor for Player {
-    type Context = SyncContext<Self>;
-}
-
-impl Player {
-    pub fn new(id: Id, x: i16, y: i16) -> Self {
-        Self {
-            id: id,
-            physics: Physics::new(x,y),
-            health: 100,
-            entity_type: EntityType::Humanoid,
-        }
-    }
-
-    pub fn expose(self) -> (Id, Physics) {
-        (self.id, self.physics)
-    }
-}
-
-// Monster
-pub struct Monster {
-    pub id: Id,
-    pub physics: Physics,
-    pub health: i16,
-    pub entity_type: EntityType,
-}
-
-impl Actor for Monster {
-     type Context = SyncContext<Self>;
- }
-
- impl Monster {
-     pub fn new(id: Id, x: i16, y: i16) -> Self {
-         Self {
-             id: id,
-             physics: Physics::new(x,y),
-             health: 100,
-             entity_type: EntityType::Monster,
-         }
-     }
-
-     pub fn expose(self) -> (Id, Physics) {
-         (self.id, self.physics)
-     }
- }
-
-// COMPONENTS
-#[derive(Clone)]
-pub struct Position {
-    pub x: i16,
-    pub y: i16,
-}
-
-impl Position {
-    pub fn new(_x: i16, _y: i16) -> Self {
-        Self {
-            x: _x,
-            y: _y,
-        }
-    }
-}
-
-impl fmt::Display for Position {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "(x: {}, y: {})", self.x, self.y)
-    }
-}
-
-#[derive(Clone)]
-pub struct Physics {
-    position: Position
-}
-
-impl Physics {
-    pub fn new(x: i16, y: i16) -> Self {
-        Self {
-            position: Position::new(x, y),
-        }
-    }
-
-    pub fn accessPosition(&self) -> &Position {
-        &self.position
-    }
-}
-
-#[derive(Clone)]
-pub enum EntityType {
-    Humanoid,
-    Monster,
-    Other
-}
-
-#[derive(Clone)]
-pub struct GameState {
-    pub physics_components: Vec<Option<Physics>>,
-    pub health_components: Vec<Option<i16>>,
-    pub humanoid_state: Vec<EntityType>,
-
-    pub players: Vec<EntityIndex>,
-}
-
-impl GameState {
-    pub fn current_state(&self) -> Self {
-        Self {
-            physics_components: self.physics_components.clone(),
-            health_components: self.health_components.clone(),
-            humanoid_state: self.humanoid_state.clone(),
-            players: self.players.clone(),
-        }
-    }
-}
-
-
-//
-// Systems
-//
-
-pub fn physics_system(game: &GameState) {
-    
-    
-
-}
-
-pub fn render_welcome() -> u16 {
-            
-    // Welcome message
-    println!("{}{}{}{}",
-        termion::clear::All,
-        termion::cursor::Goto(1,1),
-        GAME_WELCOME,
-        termion::cursor::Hide);
-
-    game_choice()
-}
-
-pub fn clean_goodbye() {
-    println!("{}{}{}",
-    termion::clear::All,
-    termion::cursor::Goto(1,1),
-    termion::cursor::Show);
-}
-
-pub fn clean_field() {
-
-    println!("{}{}{}{}",
-        termion::clear::All,
-        termion::cursor::Goto(1,1),
-        GAME_FIELD,
-        termion::cursor::Hide);
-}
-
-fn game_choice() -> u16 {
-    
-    let stdout = stdout();
-    let mut stdout = stdout.lock().into_raw_mode().unwrap();
-    
-    let mut stdin = stdin().keys();
-    let mut game_code = 0 as u16;
-
-    loop {
-    
-        let b = stdin.next();
-        if let Some(Ok(key)) = b {
-            match key {
-                Key::Char('q') => {
-                    break;
-                },
-                Key::Char('p') => {
-                    game_code = 1;
-                    break;
-                },
-                _ => {}
+            if let Some(Ok(key)) = input {
+                    tx.send(key).unwrap()
             }
         }
-    }    
-    game_code
+    });
+    rx
 }
 
-pub fn take_input() -> u16 {
-   
-    let stdout = stdout();
-    let mut stdout = stdout.lock().into_raw_mode().unwrap();
+pub fn sleep(millis: u64) {
+    let duration = time::Duration::from_millis(millis);
+    thread::sleep(duration);
+}
 
-    let mut stdin = stdin().keys();
-    let mut game_code = 3 as u16;
+pub fn pick_last_value(latest_keys: std::sync::mpsc::TryIter<termion::event::Key>) -> termion::event::Key {
+    let mut final_val = termion::event::Key::Null;
+    for x in latest_keys {
+        final_val = x;
+    }
+    final_val
+}
 
-    loop {
-    
-        let b = stdin.next();
-        if let Some(Ok(key)) = b {
-            match key {
-                Key::Char('q') => {
-                    game_code = 0;
-                    break;
-                },
-                Key::Right => {
-                    game_code = 1;
-                    break;
-                },
-                _ => {break;}
-            }
+pub enum Msg {
+    End,
+    Continue,
+}
+
+pub fn parse_input(val: termion::event::Key) -> Msg {
+    match val {
+        termion::event::Key::Char('q') => {Msg::End},
+        termion::event::Key::Char(c) => {
+            println!("{clear}{goto}{c}",
+                     clear=termion::clear::All,
+                     goto=termion::cursor::Goto(1,1),
+                     c=c);
+            Msg::Continue
         }
-    }    
-    game_code
-}
-
-pub fn render_game(game: GameState) {
-   
-    for player in game.players {
-        if let Some(physics) = game.physics_components.get(player as usize) {
-            match physics {
-                Some(physics) => {
-                    match game.humanoid_state.get(player as usize) {
-                        Some(EntityType::Humanoid) => {
-
-                            println!("{}O{}",
-                                termion::cursor::Goto(physics.position.x as u16, physics.position.y as u16),
-                                termion::cursor::Hide);
-                        },
-                        Some(EntityType::Monster) => {
-
-                            println!("{}X{}",
-                                termion::cursor::Goto(physics.position.x as u16, physics.position.y as u16),
-                                termion::cursor::Hide);
-                        },
-                        _=> {},
-                    }
-                },
-                _=> {}
-            }
-        }    
+        _=> {
+            println!("{clear}{goto}No News",
+                     clear=termion::clear::All,
+                     goto=termion::cursor::Goto(1,1));
+            Msg::Continue
+        }
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,29 @@
-use actix::sync::SyncArbiter;
-use actix::{System, Arbiter, Context, Actor, Addr};
-use futures::{future, Future};
+use std::io;
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::TryRecvError;
 use std::{thread, time};
-use std::io::{Write, stdin, stdout};
 
-use termion::event::Key;
+use termion;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use lib::{GameState, Player, Monster, take_input, render_welcome, render_game, clean_goodbye, clean_field};
 
-mod actors;
-use actors::inputer::{Inputact, CallInput};
+use lib::{parse_input, spawn_stdin_channel, pick_last_value, sleep, Msg};
 
 fn main() {
-  
-    let fut = actix::run(|| {
+    let stdin_channel = spawn_stdin_channel();
 
-        let inputer = SyncArbiter::start(2, || Inputact::new());
- 
-        let play_bool = render_welcome();
+    loop {
+        
+        let mut stdout = io::stdout().into_raw_mode().unwrap();
 
-        // Kill game on command
-        if (play_bool == 1) {
-            clean_field();
-        } else {
-            clean_goodbye();
-            std::process::exit(1);
-            return future::ok(())
+        let latest_keys = stdin_channel.try_iter();
+        let final_val = pick_last_value(latest_keys);
+        match parse_input(final_val) {
+            Msg::End => break,
+            _=> {}
         }
- 
-        // Else Set up the game
-        let player = Player::new(0, 5, 10);
-        let monster = Monster::new(1, 4, 8);
-
-        // Initialise Gamestate
-        let mut gamestate = GameState{
-            physics_components: vec![],
-            health_components: vec![],
-            humanoid_state: vec![],
-            players: vec![],
-        };
-
-        gamestate.physics_components.push(Some(player.physics));
-        gamestate.physics_components.push(Some(monster.physics));
-
-        gamestate.health_components.push(Some(player.health));
-        gamestate.health_components.push(Some(monster.health));
-       
-        gamestate.humanoid_state.push(player.entity_type);
-        gamestate.humanoid_state.push(monster.entity_type);
-
-        gamestate.players.push(player.id);
-        gamestate.players.push(monster.id);
-
-        // Initialise Game 
-        let initial_game = gamestate.current_state();
-        render_game(initial_game);
-         
-        // GameLoop
-        loop {
-
-            // copy game state into loop
-            let input_state = gamestate.current_state();
- 
-            for phys in &input_state.physics_components {
-            
-                //let res = take_input();
-                //println!("{}", res.unwrap());
-            }
-
-            gamestate = input_state.current_state();
-            
-            let wait = time::Duration::from_millis(100);
-            thread::sleep(wait);
-
-        }
-
-        std::process::exit(1);
-        future::ok(())
-    });
+        sleep(500)
+    }
 }
+
